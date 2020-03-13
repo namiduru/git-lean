55f40cedd161c4e76836302da1f817f82996828b
Mehmet NAMIDURU <mehmetnamiduru1@jotforms-MacBook-Pro-9.local>
2020-03-13 21:07:02 +0300

    Add Execution Mechanism of git-lean commands

--- src/commands.js
+++ src/commands.js
@@ -1,2 +1,12 @@
-const stash = 'git stash --include-untracked && git fetch origin master && git reset --hard origin/master';
-const stashAndPop = 'stash && git stash pop';
+const gitCommands = {
+  stash: {
+    command: 'git stash --include-untracked && git fetch origin master && git reset --hard origin/master',
+    message: 'Executing: git-lean stash'
+  },
+  stashAndPop: {
+    command: this.stash + ' && git stash pop',
+    message: 'Executing: git-lean stashAndPop'
+  }
+}
+
+module.exports = gitCommands;
--- src/git-lean.js
+++ src/git-lean.js
@@ -1,3 +1,20 @@
+const { exec } = require('child_process');
 const chalk = require('chalk');
+var argv = require('minimist')(process.argv.slice(2));
 
-console.log(chalk.blue('Hello Welcome to the git-lean...'));
+const gitCommands = require('./commands');
+
+// Exec Command
+if (gitCommands[argv._[0]]) {
+  exec(gitCommands[argv._[0]].command, (err, stdout, stderr) => {
+    if (err) {
+      // It is not a git repository
+    } else {
+      console.log(chalk.green(stdout));
+      console.log(chalk.red(stderr));
+      console.log(chalk.blue(gitCommands[argv._[0]].message));
+    }
+  });
+}
+
+// console.log(gitCommands);

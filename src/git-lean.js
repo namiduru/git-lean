eb9f94323095461375bb5d2fc769c476790f0c39
Mehmet NAMIDURU <mehmetnamiduru1@jotforms-MacBook-Pro-9.local>
2020-06-09 23:04:47 +0300

    generate command dynamically

--- src/git-lean.js
+++ src/git-lean.js
@@ -6,7 +6,7 @@ const gitCommands = require('./commands');
 
 // Exec Command
 if (gitCommands[argv._[0]]) {
-  exec(gitCommands[argv._[0]].command, (err, stdout, stderr) => {
+  exec(generateFunctionCommand(argv._), (err, stdout, stderr) => {
     if (err) {
       // It is not a git repository
     } else {
@@ -15,6 +15,20 @@ if (gitCommands[argv._[0]]) {
       console.log(chalk.blue(gitCommands[argv._[0]].message));
     }
   });
+} else {
+  console.log(chalk.red('Something went wrong!'));
 }
 
-// console.log(gitCommands);
+function generateFunctionCommand(arguments) {
+  let command = gitCommands[arguments[0]].command;
+
+  if(arguments.length >=1) {
+    for (let i = 1; i < arguments.length; i++) {
+      let placeholder = arguments[i];
+
+      command = command.replace('{' + i -1 + '}', placeholder);
+    }
+  }
+
+  return command;
+}
